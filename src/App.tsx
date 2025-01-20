11dec0bb9316cd703683631e27216865331e9282
maple <mapleia01@gmail.com>
2025-01-20 01:51:32 -0800

    use hash url

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 // @deno-types="npm:@types/react@18"
 import { useState } from "react";
-import { Route, Switch } from "wouter";
+import { Route, Router, Switch } from "wouter";
 
 import { mainTheme } from "./components/styling/index.ts";
 import { SpecContext } from "./data/spec.context.tsx";
@@ -10,6 +10,7 @@ import NightmarePage from "./pages/nightmare.page.tsx";
 import { CMId } from "./gw2/cms.ts";
 import { Specs, SpecType, SpecTypeMap } from "./gw2/type.ts";
 import ShatteredPage from "./pages/shattered.page.tsx";
+import { useHashLocation } from "wouter/use-hash-location";
 
 function App() {
   const [specType, setSpecType] = useState<SpecType>(SpecType.POWER);
@@ -31,13 +32,11 @@ function App() {
         }}
       >
         <Switch>
-          <Route path={`/${CMId.Nightmare}`} nest>
-            <NightmarePage />
-          </Route>
-          <Route path={`/${CMId.ShatteredObservatory}`} nest>
-            <ShatteredPage />
-          </Route>
-          <HomePage />
+          <Router hook={useHashLocation}>
+              <Route path={`/${CMId.Nightmare}`} component={NightmarePage} nest/>
+              <Route path={`/${CMId.ShatteredObservatory}`} component={ShatteredPage} nest/>
+              <Route path="/" component={HomePage} />
+          </Router>
         </Switch>
       </SpecContext.Provider>
     </ThemeProvider>

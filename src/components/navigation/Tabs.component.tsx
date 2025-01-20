de9793edf5f913bc3352e011ea805bc7a7186ee2
maple <mapleia01@gmail.com>
2025-01-19 19:05:47 -0800

    use theme prop

--- src/components/navigation/Tabs.component.tsx
+++ src/components/navigation/Tabs.component.tsx
@@ -13,7 +13,7 @@ export const NavigationTabs = styled((props: StyledTabsProps) => (
     {...props}
     TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }}
   />
-))(({ theme }: {theme: Theme}) => ({
+))(({ theme }: { theme: Theme }) => ({
   "& .MuiTabs-indicator": {
     display: "flex",
     justifyContent: "center",
@@ -33,7 +33,7 @@ interface StyledTabProps {
 
 export const NavigationTab = styled((props: StyledTabProps) => (
   <Tab disableRipple {...props} />
-))(({ theme }: {theme: Theme}) => ({
+))(({ theme }: { theme: Theme }) => ({
   marginRight: 8,
   textTransform: "uppercase",
   fontSize: theme.typography.pxToRem(18),

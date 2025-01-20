a9a8ded9e75634d789ae04b9c4c3cc704adb5d10
maple <mapleia01@gmail.com>
2025-01-19 19:01:44 -0800

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,37 @@
-import { Card, CssBaseline, Grid2, ThemeProvider } from "@mui/material";
-import { Specs } from "./components/gw2/type.ts";
-import { theme } from "./components/styling/index.ts";
-import Sidebar from "./components/navigation/Sidebar.component.tsx";
-import NavigationBar from "./components/navigation/bar.component.tsx";
-import { SpecContext } from "./data/spec.context.tsx";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { useState } from "react";
-import SetupTable from "./components/gw2/SetupTable.component.tsx";
+import { Route, Switch } from "wouter";
+
+import { Specs, SpecType, SpecTypeMap, CMId } from "@gw2";
+import { mainTheme } from "./components/styling/index.ts";
+import { SpecContext } from "./data/spec.context.tsx";
+import HomePage from "./pages/home.page.tsx";
+import NightmarePage from "./pages/nightmare.page.tsx";
 
 function App() {
+  const [specType, setSpecType] = useState<Specs>(SpecType.POWER);
   const [activeSpec, setSpec] = useState<Specs>(Specs.SLB);
 
   const handleChange = (_event: React.SyntheticEvent, newValue: Specs) => {
     setSpec(newValue);
+    setSpecType(SpecTypeMap[newValue]);
   };
 
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={mainTheme}>
       <CssBaseline />
       <SpecContext.Provider
         value={{
           activeSpec,
           setSpec: handleChange,
           specList: [Specs.SLB, Specs.HARB],
+          specType,
         }}
       >
-        <NavigationBar websiteName="Awooo" />
-        <Grid2
-          container
-          columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-          sx={{ mx: 3, my: 1, flexShrink: 1, flexGrow: 0, flexBasis: "auto" }}
-        >
-          <Grid2 size={2}>
-            <Sidebar isPower isCondi />
-          </Grid2>
-          <Grid2
-            size={10}
-            sx={{ flexShrink: 0, flexGrow: 1, flexBasis: "auto" }}
-            bgcolor={theme.palette.background.paper}
-          >
-            <SetupTable
-              relicId={100090}
-              sigilIds={[24615, 24868]}
-              consumablesIds={[24615, 24868]}
-            />
-          </Grid2>
-        </Grid2>
+          <Switch>
+            <Route path="/" component={HomePage}/>
+            <Route path={`/${CMId.Nightmare}`} component={NightmarePage}/>
+          </Switch>
       </SpecContext.Provider>
     </ThemeProvider>
   );

3842cc0c39bb833eada9fc05deb4f8731c9afd9c
maple <mapleia01@gmail.com>
2025-01-19 22:18:07 -0800

    fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
+// @deno-types="npm:@types/react@18"
 import { useState } from "react";
 import { Route, Switch } from "wouter";
 
@@ -8,16 +9,16 @@ import HomePage from "./pages/home.page.tsx";
 import NightmarePage from "./pages/nightmare.page.tsx";
 import { CMId } from "./gw2/cms.ts";
 import { Specs, SpecType, SpecTypeMap } from "./gw2/type.ts";
+import ShatteredPage from "./pages/shattered.page.tsx";
 
 function App() {
-  const [specType, setSpecType] = useState<Specs>(SpecType.POWER);
+  const [specType, setSpecType] = useState<SpecType>(SpecType.POWER);
   const [activeSpec, setSpec] = useState<Specs>(Specs.SLB);
 
   const handleChange = (_event: React.SyntheticEvent, newValue: Specs) => {
     setSpec(newValue);
     setSpecType(SpecTypeMap[newValue]);
   };
-
   return (
     <ThemeProvider theme={mainTheme}>
       <CssBaseline />
@@ -30,8 +31,13 @@ function App() {
         }}
       >
         <Switch>
-          <Route path="/" component={HomePage} />
-          <Route path={`/${CMId.Nightmare}`} component={NightmarePage} />
+          <Route path={`/${CMId.Nightmare}`} nest>
+            <NightmarePage />
+          </Route>
+          <Route path={`/${CMId.ShatteredObservatory}`} nest>
+            <ShatteredPage />
+          </Route>
+          <HomePage />
         </Switch>
       </SpecContext.Provider>
     </ThemeProvider>

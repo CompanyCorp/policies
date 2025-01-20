7612944ac682b567cf45ff1cc799d1fdf9c5eb88
maple <mapleia01@gmail.com>
2025-01-20 03:57:27 -0800

    change path util

--- src/components/navigation/utils.ts
+++ src/components/navigation/utils.ts
@@ -2,19 +2,22 @@
 import { useContext } from "react";
 import { SpecContext } from "../../data/spec.context.tsx";
 import {
-  condiFights,
   isCondiFight,
   isPowerFight,
-  powerFights,
+  useCondiFights,
+  usePowerFights,
 } from "../../gw2/cms.ts";
 import { SpecType, SpecTypeMap } from "../../gw2/type.ts";
 import { useRouter } from "wouter";
 
-export const parseLocation = (input: string) => {
+export const parseLocation = (input: string, user?: string) => {
   let page = input;
   // if forward-slash exists, remove it
-  if (input.charAt(0) === "/") {
-    page = page.slice(1);
+  if (page.includes("/")) {
+    const pages = page.split("/");
+
+    page = pages[pages.length - 1];
+    console.log("parseLocation", pages, user);
   }
   // if ? exists, remove it and everything after it
   if (page.includes("?")) {
@@ -32,21 +35,23 @@ export const parseLocation = (input: string) => {
  *  return a power fight URL
  */
 export const useRedirectSpec = (path: string) => {
-  let page = parseLocation(path);
+  let page = parseLocation(path, "useRedirectSpec");
   // if page is a base page, get the actual page from the router
   if (page === "") {
     const router = useRouter();
-    page = parseLocation(router.base);
+    page = parseLocation(router.base, "useRedirectSpec '' ");
   }
   const { activeSpec } = useContext(SpecContext);
   const specIsPower = SpecTypeMap[activeSpec] === SpecType.POWER;
   const specIsCondi = SpecTypeMap[activeSpec] === SpecType.CONDI;
   if (specIsPower) {
     const pageIsCondi = isCondiFight(page);
+    const powerFights = usePowerFights();
     return pageIsCondi ? powerFights[0].href || null : null;
   }
   if (specIsCondi) {
     const pageIsPower = isPowerFight(page);
+    const condiFights = useCondiFights();
     return pageIsPower ? condiFights[0].href || null : null;
   }
   return null;
